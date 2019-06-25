12a5d0b56c63c1363753cd330fe83ee015e90345
wangxiaoliang <15618359568@163.com>
2019-06-25 17:48:50 +0800

    部署和查询 填写表单 执行 添加表单

--- src/main/java/com/brighton/flowableHalloWord/HolidayRequest.java
+++ src/main/java/com/brighton/flowableHalloWord/HolidayRequest.java
@@ -2,8 +2,15 @@ package com.brighton.flowableHalloWord;
 
 import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
-import org.flowable.idm.engine.impl.cfg.StandaloneIdmEngineConfiguration;
+import org.flowable.engine.repository.Deployment;
+import org.flowable.engine.repository.ProcessDefinition;
+import org.flowable.engine.runtime.ProcessInstance;
+
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * <p>
@@ -19,7 +26,55 @@ public class HolidayRequest {
                 .setJdbcUsername("root")
                 .setJdbcPassword("112233")
                 .setJdbcDriver("com.mysql.jdbc.Driver")
+                // true 数据库表结构不存在时，会创建相应的表结构
                 .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
         ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
+        // 部署流程定义
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("holiday-request.bpmn20.xml")
+                .name("holiday-request")
+                .deploy();
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        System.out.println("流程引擎: " + processEngine);
+        System.out.println("流程部署id: " + deployment.getId());
+        System.out.println("流程部署名称: " + deployment.getName());
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+        // 查询流程部署
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deployment.getId())
+                .singleResult();
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        System.out.println("Found process definition: " + processDefinition.getId());
+        System.out.println("Found process definition: " + processDefinition.getName());
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        // 填写表单
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        System.out.println("填写表单");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Who are you?");
+        String employee = scanner.nextLine();
+
+        System.out.println("How many holidays do you want to request?");
+        Integer nrOfHoliday = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Why do you need them?");
+        String description = scanner.nextLine();
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        // 执行流程定义
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        // 添加表单数据
+        HashMap<String, Object> variables = new HashMap<String, Object>();
+        variables.put("employee", employee);
+        variables.put("nrOfHoliday", nrOfHoliday);
+        variables.put("description", description);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("holidayRequest", variables);
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        System.out.println("流程定义id: " + processInstance.getProcessDefinitionId());
+        System.out.println("流程实例id: " + processInstance.getId());
+        System.out.println("流程活动id: " + processInstance.getActivityId());
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
     }
 }
